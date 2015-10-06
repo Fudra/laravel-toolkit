a5673491e5903395e77873368e0af5936c626c45
Philipp Wahle <phil.wahle@student.htw-berlin.de>
2015-10-06 16:05:03 +0200

    Toolbox
    Add Form and ModelHelper

---
+++ Helpers/ModelHelper.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Helpers;
+
+
+trait ModelHelper
+{
+
+    /**
+     * Return all active Items from Model
+     *
+     * @param $query
+     * @return mixed
+     */
+    // TODO: Rename in active
+    public function scopeActive($query)
+    {
+        return $query->where('active', 1);
+    }
+
+    /**
+     * Order all Items
+     *
+     * @param $query
+     * @return mixed
+     */
+    public function scopeOrder($query)
+    {
+        return $query->orderBy('order');
+    }
+
+    /**
+     * Find Element by Slug
+     *
+     * @param $query
+     * @param $slug
+     * @return mixed
+     */
+    public function scopeFindBySlug($query, $slug)
+    {
+        return $query->where('slug', $slug)->firstOrFail();
+    }
+}
---
+++ views/partials/form/select.blade.php
@@ -0,0 +1,6 @@
+<div class="input-group">
+    <span class="input-group-addon">
+       {!! Form::checkbox($name, $select, true) !!}
+    </span>
+    <label class="form-control" for="{{$name}}">{{$label}}</label>
+</div>
