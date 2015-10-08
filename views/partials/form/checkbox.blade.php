2c8564c6ad206b6807eae9d3375482ae9380d734
Philipp Wahle <phil.wahle@student.htw-berlin.de>
2015-10-08 13:25:48 +0200

    add form inputs

--- views/partials/form/checkbox.blade.php
+++ views/partials/form/checkbox.blade.php
@@ -1,6 +1,6 @@
 <div class="input-group">
     <span class="input-group-addon">
-       {!! Form::checkbox($name, $select, true) !!}
+       {!! Form::checkbox($label, $select, true) !!}
     </span>
     <label class="form-control" for="{{$name}}">{{$label}}</label>
-</div>
+</div> 
